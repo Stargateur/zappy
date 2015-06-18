bca2d002a1fd281c67017133308062e44a3b7808
degand_a <alaric.degand@epitech.eu>
2015-06-18 17:39:24 +0200

    creation fenetre SDL + grille

---
+++ tmpGraph/main.c
@@ -0,0 +1,71 @@
+/*
+** main.c for  in /home/degand/Projects/PSU_2014_zappy/tmpGraph
+** 
+** Made by Alaric
+** Login   <degand@epitech.net>
+** 
+** Started on  Thu Jun 18 15:58:26 2015 Alaric
+** Last update Thu Jun 18 17:33:14 2015 Alaric
+*/
+
+#include	<SDL/SDL_ttf.h>
+#include	<SDL/SDL.h>
+#include	<SDL/SDL_image.h>
+#include	<unistd.h>
+#include	"color.h"
+
+SDL_Surface	*init_grid()
+{
+  SDL_Surface	*grid;
+  RGBcolor	white;
+
+  grid = SDL_CreateRGBSurface(SDL_HWSURFACE, 1001, 1001, 32, 0, 0, 0, 0);
+  white.r = 255;
+  white.g = 255;
+  white.b = 255;
+  white.x = 0;
+  while (white.x <= 1000)
+    {
+      white.y = 0;
+      while (++white.y < 1000)
+        colorPixel(grid, white);
+      white.x += 1000 / 40;
+    }
+  white.y = 0;
+  while (white.y <= 1000)
+    {
+      white.x = 0;
+      while (++white.x < 1000)
+        colorPixel(grid, white);
+      white.y += 1000 / 40;
+    }
+  return (grid);
+}
+
+int		main()
+{
+  SDL_Surface	*ecran;
+  SDL_Surface	*grid;
+  SDL_Rect	pos;
+
+  if (SDL_Init(SDL_INIT_VIDEO) == -1)
+    {
+      printf("Init Fail\n");
+      return (-1);
+    }
+  ecran = SDL_SetVideoMode(1001, 1001, 32, SDL_HWSURFACE);
+  if (ecran == NULL)
+    {
+      printf("Set Fail\n");
+      return (-1);
+    }
+  SDL_WM_SetCaption("zappy", NULL);
+  pos.x = 0;
+  pos.y = 0;
+  grid = init_grid();
+  SDL_BlitSurface(grid, NULL, ecran, &pos);
+  SDL_Flip(ecran);
+  sleep(5);
+  SDL_Quit();
+  return (0);
+}
