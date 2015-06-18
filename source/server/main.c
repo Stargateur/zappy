ac630d3c5f1b95c89d5e3df486b11f22a49ff20f
plasko_a <plasko_a@epitech.eu>
2015-06-18 18:08:59 +0200

    ouverture du server port par default 4242

--- source/server/main.c
+++ source/server/main.c
@@ -5,17 +5,49 @@
 ** Login   <plasko_a@epitech.eu>
 ** 
 ** Started on  Tue Jun 16 16:38:42 2015 Antoine Plaskowski
-** Last update Thu Jun 18 17:56:10 2015 Antoine Plaskowski
+** Last update Thu Jun 18 18:07:15 2015 Antoine Plaskowski
 */
 
-#include	<getopt.h>
+#include        <unistd.h>
+#include        <signal.h>
+#include        <stdio.h>
+#include	<sys/socket.h>
 #include	"parse_opt.h"
+#include	"create_binded_socket.h"
+
+bool    g_keep_running = true;
+
+static void     int_handler(int const signum)
+{
+  (void)signum;
+  g_keep_running = false;
+}
+
+static int	init_socket(char const * const port)
+{
+  int		sfd;
+
+  if (signal(SIGINT, int_handler) == SIG_ERR)
+    perror("signal()");
+  if ((sfd = create_binded_socket(port)) == -1)
+    return (-1);
+  if (listen(sfd, 42))
+    {
+      perror("listen()");
+      return (-1);
+    }
+  return (sfd);
+}
 
 int		main(int argc, char **argv)
 {
+  int		sfd;
   t_opt		opt;
 
   parse_opt(argv, argc, &opt);
   show_opt(&opt);
+  if ((sfd = init_socket(opt.p)) == -1)
+    return (1);
+  close(sfd);
   return (0);
 }
