c827973614c8b0b35828729e54dce4e28f86f64d
degand_a <alaric.degand@epitech.eu>
2015-06-19 19:55:01 +0200

    SDL : Grille OK

--- tmpGraph/main.c
+++ tmpGraph/main.c
@@ -5,7 +5,7 @@
 ** Login   <degand@epitech.net>
 ** 
 ** Started on  Thu Jun 18 15:58:26 2015 Alaric
-** Last update Fri Jun 19 19:35:30 2015 Alaric
+** Last update Fri Jun 19 19:54:13 2015 Alaric
 */
 
 //#include	<SDL/SDL_ttf.h>
@@ -13,46 +13,58 @@
 //#include	<SDL/SDL_image.h>
 #include	<unistd.h>
 #define		SHAPE_SIZE 24
+#define		SIZE_X 1000
+#define		SIZE_Y 1000
+
+void		draw_grid(SDL_Renderer *renderer)
+{
+  int		a;
+
+  a = 0;
+  while (a <= 1000)
+    {
+      SDL_RenderDrawLine(renderer, a, 0, a, 1000);
+      a += SIZE_X / 40;
+    }
+  a = 0;
+  while (a <= 1000)
+    {
+      SDL_RenderDrawLine(renderer, 0, a, 1000, a);
+      a += SIZE_Y / 40;
+    }
+}
 
 int		main()
 {
   SDL_Window	*fenetre;
   SDL_Renderer	*renderer;
   SDL_Texture	*Linemate;
-  //SDL_Surface	*grid;
-  SDL_Surface	*linemate;
-  SDL_Rect	SrcR;
+  SDL_Surface	*loader;
   SDL_Rect	DestR;
 
-  SrcR.x = 0;
-  SrcR.y = 0;
-  SrcR.w = SHAPE_SIZE;
-  SrcR.h = SHAPE_SIZE;
-
-  DestR.x = 0;
-  DestR.y = 0;
+  DestR.x = 1;
+  DestR.y = 1;
   DestR.w = SHAPE_SIZE;
   DestR.h = SHAPE_SIZE;
 
-  //fenetre = init_video();
   //SDL_CreateWindowAndRenderer(0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP, &fenetre, &renderer);
-  fenetre = SDL_CreateWindow("SDL_RenderCopy Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1000, 1000, 0);
+  fenetre = SDL_CreateWindow("SDL_RenderCopy Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SIZE_X + 1, SIZE_Y + 1, 0);
   renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED);
-  //SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
-  //SDL_RenderSetLogicalSize(renderer, 640, 580);
-  linemate = Bmp_Loader("BMP/Linemate.bmp");
+  //load image -> tranfert texture -> free image
+  loader = Bmp_Loader("BMP/Linemate.bmp");
   Linemate = SDL_CreateTextureFromSurface(renderer, linemate);
-  //grid = init_grid();
+  SDL_FreeSurface(loader);
+  //clear de la fenetre
   SDL_RenderClear(renderer);
-  SDL_RenderCopy(renderer, Linemate, &SrcR, &DestR);
-
+  //affichage BMP => a modif pour toutes les images
+  SDL_RenderCopy(renderer, Linemate, NULL, &DestR);
+  //Changement couleur pour la grille
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  SDL_RenderDrawLine(renderer, 25, 0, 25, 1000);
-  SDL_RenderDrawLine(renderer, 0, 25, 1000, 25);
-  //SDL_RenderCopy(renderer, Linemate, NULL, NULL);
+  draw_grid(renderer);
+  //actualisation de l'affichage
   SDL_RenderPresent(renderer);
 
-  sleep(2);
+  sleep(5);
 
   SDL_Quit();
   return (0);
