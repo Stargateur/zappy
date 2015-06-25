7515ada99d4413d260f8604eb8d20b0a04c35589
costa_b <costa_b@epitech.eu>
2015-06-25 17:43:02 +0200

    input

--- tmpGraph/input.c
+++ tmpGraph/input.c
@@ -5,7 +5,7 @@
 ** Login   <costa_b@epitech.net>
 ** 
 ** Started on  Mon Jun 22 15:34:38 2015 Kevin Costa
-** Last update Thu Jun 25 16:44:33 2015 Kevin Costa
+** Last update Thu Jun 25 17:42:39 2015 Kevin Costa
 */
 
 #include	<SDL2/SDL.h>
@@ -21,18 +21,26 @@ void input(void)
 	a = 0; /* QUITTER */
       if (event.type == SDL_KEYDOWN)
 	{
+	  if (event.key.keysym.sym == SDLK_SPACE)
+	    a = 0; /* CHECK CASE */
 	  if (event.key.keysym.sym == SDLK_ESCAPE)
 	    a = 0; /* QUITTER */
 	  if (event.key.keysym.sym == SDLK_UP)
-	    a = 0; /* ZOOM + */
+	    a = 0; /* HAUT */
 	  if (event.key.keysym.sym == SDLK_DOWN)
-	    a = 0; /* ZOOM - */
+	    a = 0; /* BAS */
 	  if (event.key.keysym.sym == SDLK_LEFT)
 	    a = 0; /* DROITE */
 	  if (event.key.keysym.sym == SDLK_RIGHT)
 	    a = 0; /* GAUCHE */
+	  if (event.key.keysym.sym == SDLK_R)
+	    a = 0; /* reset camera pos initial */
 	  if (event.key.keysym.sym == SDLK_SPACE)
 	    a = 0; /* CHECK CASE */
+	  if (event.key.keysym.sym == SDLK_KP_MINUS)
+	    a = 0; /* ZOOM - */
+	  if (event.key.keysym.sym == SDLK_KP_PLUS)
+	    a = 0; /* ZOOM + */
 	}
     }
 }
