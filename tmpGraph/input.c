bb1b4ae6885f8df2f648335740b31c122ba210b8
costa_b <costa_b@epitech.eu>
2015-06-26 14:43:28 +0200

    t

--- tmpGraph/input.c
+++ tmpGraph/input.c
@@ -5,7 +5,7 @@
 ** Login   <costa_b@epitech.net>
 ** 
 ** Started on  Mon Jun 22 15:34:38 2015 Kevin Costa
-** Last update Fri Jun 26 14:38:06 2015 Kevin Costa
+** Last update Fri Jun 26 14:43:24 2015 Kevin Costa
 */
 
 #include	<SDL2/SDL.h>
@@ -23,38 +23,28 @@ void input(void)
 	{
 	case SDL_QUIT:
 	  exit(1); /* quitter */
-	  break;
 	case SDL_MOUSEBUTTONDOWN:
 	  x = event.button.x;
 	  y = event.button.y;
-	  break;
 	case SDL_KEYDOWN:
 	  switch (event.key.keysym.scancode)
 	    {
 	    case SDL_SCANCODE_ESCAPE:
 	      exit(1); /* quitter */
-	      break;
 	    case SDL_SCANCODE_UP:
 	      a = 0; /* HAUT */
-	      break;
 	    case SDL_SCANCODE_DOWN:
 	      a = 0; /* BAS */
-	      break;
 	    case SDL_SCANCODE_RIGHT:
 	      a = 0; /* GAUCHE */
-	      break;
 	    case SDL_SCANCODE_LEFT:
 	      a = 0; /* DROITE */
-	      break;
 	    case SDL_SCANCODE_KP_PLUS:
 	      a = 0; /* ZOOM + */
-	      break;
 	    case SDL_SCANCODE_KP_MINUS:
 	      a = 0; /* ZOOM - */
-	      break;
 	    case SDL_SCANCODE_R:
 	      a = 0; /* reset cam */
-	      break;
 	    }
 	}
     }
