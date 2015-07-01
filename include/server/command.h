8a67c138a19bcb246902d3b074bcfc8afd33f5f3
plasko_a <plasko_a@epitech.eu>
2015-07-01 05:31:05 +0200

    avec le point h c'est meixu

---
+++ include/server/command.h
@@ -0,0 +1,20 @@
+/*
+** command.h for command in /home/plasko_a/projet/epitech/PSU_2014_zappy
+** 
+** Made by Antoine Plaskowski
+** Login   <antoine.plaskowski@epitech.eu>
+** 
+** Started on  Wed Jul  1 05:24:19 2015 Antoine Plaskowski
+** Last update Wed Jul  1 05:25:33 2015 Antoine Plaskowski
+*/
+
+#ifndef		COMMAND_H_
+# define	COMMAND_H_
+
+# include	<stdbool.h>
+# include	"game.h"
+# include	"client.h"
+
+bool		get_cmd(t_game *game, t_client *client);
+
+#endif		/* !COMMAND_H_ */
