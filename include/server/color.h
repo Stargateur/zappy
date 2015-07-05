19c904970063e364828eb0cbd7a6bb5ccfc952d2
plasko_a <plasko_a@epitech.eu>
2015-07-05 23:52:21 +0200

    norme

--- include/server/color.h
+++ include/server/color.h
@@ -5,17 +5,17 @@
 ** Login   <degand@epitech.net>
 ** 
 ** Started on  Thu Jun 18 16:48:37 2015 Alaric
-** Last update Thu Jun 18 16:56:08 2015 Alaric
+** Last update Sun Jul  5 23:52:16 2015 Antoine Plaskowski
 */
 
 #ifndef		COLOR_H_
 # define	COLOR_H_
 
-typedef struct  RGBcolor
+typedef	struct	RGBcolor
 {
-  int           r;
-  int           g;
-  int           b;
+  int		r;
+  int		g;
+  int		b;
   int		x;
   int		y;
 }               RGBcolor;
