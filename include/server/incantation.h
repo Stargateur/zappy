f7d9b1d5d79f3d082255e5620db5d91b8a492b70
plasko_a <plasko_a@epitech.eu>
2015-07-05 18:00:34 +0200

    elevation en cours #code sale

--- include/server/incantation.h
+++ include/server/incantation.h
@@ -5,12 +5,16 @@
 ** Login   <antoine.plaskowski@epitech.eu>
 ** 
 ** Started on  Sat Jul  4 22:48:39 2015 Antoine Plaskowski
-** Last update Sat Jul  4 22:54:15 2015 Antoine Plaskowski
+** Last update Sun Jul  5 17:52:24 2015 Antoine Plaskowski
 */
 
 #ifndef		INCANTATION_H_
 # define	INCANTATION_H_
 
+#include	"game.h"
+#include	"player.h"
+
+bool		incantation_en_cours(t_player *player);
 bool		incantation(t_game *game, t_player *player, char *str);
 
 #endif		/* !INCANTATION_H_ */
--- source/server/incantation.c
+++ source/server/incantation.c
@@ -5,7 +5,7 @@
 ** Login   <antoine.plaskowski@epitech.eu>
 ** 
 ** Started on  Sat Jul  4 22:43:36 2015 Antoine Plaskowski
-** Last update Sun Jul  5 11:03:12 2015 Antoine Plaskowski
+** Last update Sun Jul  5 17:59:42 2015 Antoine Plaskowski
 */
 
 #include	<stdlib.h>
@@ -14,15 +14,26 @@
 #include	"incantation.h"
 #include	"message.h"
 
+bool		incantation_en_cours(t_player *player)
+{
+  if (player == NULL)
+    return (true);
+  if (player->action == NULL)
+    return (true);
+  if (player->action->cmd == NULL)
+    return (true);
+  if (player->action->cmd->type == INCANTATION)
+    add_string(player->client, INCANTATION_EN_COURS);
+  return (false);
+}
+
 bool		incantation(t_game *game, t_player *player, char *arg)
 {
   char		*str;
 
   if (game == NULL || player == NULL || arg == NULL)
     return (true);
-  add_string(player->client, INCANTATION);
-  if (player_levelup(game, &game->map, player, true) == true)
-    return (true);
+  player_levelup(game, &game->map, player, true);
   str = malloc_vsnprintf("niveau actuel : %lu\n", player->range);
   add_string(player->client, str);
   free(str);
