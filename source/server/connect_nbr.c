b5986a02d5b881733aa4a538f8441ed1c3dec590
plasko_a <plasko_a@epitech.eu>
2015-07-05 04:36:37 +0200

    fix

--- source/server/connect_nbr.c
+++ source/server/connect_nbr.c
@@ -5,7 +5,7 @@
 ** Login   <antoine.plaskowski@epitech.eu>
 ** 
 ** Started on  Sat Jul  4 22:43:36 2015 Antoine Plaskowski
-** Last update Sun Jul  5 04:33:20 2015 Antoine Plaskowski
+** Last update Sun Jul  5 04:36:06 2015 Antoine Plaskowski
 */
 
 #include	"game.h"
@@ -20,7 +20,7 @@ bool		connect_nbr(t_game *game, t_player *player, char *arg)
   if (game == NULL || player == NULL || arg == NULL)
     return (true);
   connect_nbr = player_team_online(game->player, player->team->team);
-  connect_nbr = player->team->connect_max - connect_nbr - 1;
+  connect_nbr = player->team->connect_max - connect_nbr;
   if (write_num_client(player->client, connect_nbr) == true)
     return (true);
   return (false);
